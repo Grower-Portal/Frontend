b0399863568b4f764fcf6aa9c734ec528a1ffe92
Abhishek Karayala <abhishekkarayala@gmail.com>
2023-12-19 19:49:14 -0600

    Added Descprition and CSS to Steps to Follow Page

--- src/pages/StepsToFollow.jsx
+++ src/pages/StepsToFollow.jsx
@@ -3,22 +3,35 @@ import '../styles/StepsToFollow.css';
 import Layout from '../components/Layout';
 
 function StepsToFollow() {
-
     return (
         <Layout>
-        <div>
-            <h1>Steps to Follow - Grower Portal</h1>
+            <div className="steps-to-follow">
+                <h1>How to Fill Out Your Application - Grower Portal</h1>
 
-            <div id="steps-container">
-                <h2>Steps to Follow</h2>
-
-                <div className="step">1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
-                <div className="step">2. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-                <div className="step">3. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</div>
-                <div className="step">4. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.</div>
-                <div className="step">5. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.</div>
+                <ol id="steps-container">
+                    <li>
+                        Access the Application Form: Navigate to the "Add Application" section on the Grower Portal.
+                    </li>
+                    <li>
+                        Enter Basic Information: Fill in basic details such as your Farm Number and Tract Number.
+                    </li>
+                    <li>
+                        Provide Detailed Information: Enter specific details about your farm, including Commodity Type, Grower Field Name, and FSA Physical Location.
+                    </li>
+                    <li>
+                        Upload Necessary Documents: Attach any required documents, such as proof of ownership or land use documentation, in the Document Upload section.
+                    </li>
+                    <li>
+                        Review Your Application: Ensure all the information provided is accurate and complete.
+                    </li>
+                    <li>
+                        Submit Your Application: Click the "Submit Application" button. You will receive a confirmation once your application has been successfully submitted.
+                    </li>
+                    <li>
+                        Await Confirmation: After submission, your application will be reviewed, and you will be notified about the approval status via email or on the portal.
+                    </li>
+                </ol>
             </div>
-        </div>
         </Layout>
     );
 }
